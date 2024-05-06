e6f5af15850362fc4fd8aaa1b808b38c9144a91f
joonyeongs <147701338+joonyeongs@users.noreply.github.com>
2024-05-06 16:48:14 +0900

    [fix] bug in preprocessing dialogues

---
+++ utils/create_prompt_and_chosen.py
@@ -0,0 +1,42 @@
+import json
+
+def clean_data(string_to_clean):
+    # Remove \"INFP\": and \"User\":
+    start_index = string_to_clean.find(":")
+    string_to_clean = string_to_clean[start_index + 1:]
+
+    # Remove *chuckles*
+    first_star_index = string_to_clean.find('*')
+    second_star_index = string_to_clean.find('*', first_star_index + 1)
+    if second_star_index != -1:
+        string_to_clean = string_to_clean[second_star_index+1:]
+
+    # Remove \" at the end of the string
+    string_to_clean = string_to_clean.strip()
+    string_to_clean = string_to_clean.lstrip('\"')
+    string_to_clean = string_to_clean.rstrip('\"')
+
+    return string_to_clean
+
+def create_pairs(dialogue):
+    pairs = []
+    for i in range(0, len(dialogue), 2):
+        if i+1 < len(dialogue):
+            pairs.append({"prompt": dialogue[i], "chosen": dialogue[i+1], })
+
+    return pairs
+
+
+def preprocess_data(dataset):
+    prompt_and_chosen_data = []
+
+    for data in dataset:
+        if not data["dialogue"][0].startswith("\"User\":"):
+            data["dialogue"] = data["dialogue"][1:]
+        
+        cleaned_dialogue = [clean_data(string_to_clean) for string_to_clean in data["dialogue"]]  
+      
+        prompt_and_chosen = create_pairs(cleaned_dialogue)
+        prompt_and_chosen_data.extend(prompt_and_chosen)
+    
+    return prompt_and_chosen_data
--- utils/generate_dialogue_infp_rejected.py
+++ utils/generate_dialogue_infp_rejected.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from joblib import Parallel, delayed
+from create_prompt_and_chosen import preprocess_data
 
 import os
 import anthropic
@@ -7,32 +8,18 @@ import json
 import time
 import sys
 
-def concatenate_dialogues(dialogues):
-    return '\n'.join(dialogues)
-
-def generate_dialgue(infp_dialogue):
-    dialogue_to_invert=concatenate_dialogues(infp_dialogue["dialogue"]),
-    start_of_dialogue=infp_dialogue["dialogue"][0]
-    character = infp_dialogue["character"]
-    situation = infp_dialogue["scene"]
-
+def reverse_response(prompt, chosen):
     system_prompt='''
-    Create in the following format where the conversation utterances are surrounded by quotes:
-    "User": "Detailed utterance 1"
-    "INFP": "Detailed utterance 2"
-
-    In this conversation, "User" is an arbitrary person and "INFP" is the INFP character. They have already been introduced and are familiar with each other. The conversation should be a back-and-forth dialogue. Each person should speak between 4 to 10 times.
-    Following this format is mandatory and very important.
+    Given a dialogue between an INFP individual and an arbitrary user, please invert the INFP's responses to reflect opposite personality traits.
     '''
 
-    prompt = f'''Given a dialogue between an INFP individual and an arbitrary user, please invert the INFP's responses to reflect opposite personality traits.
-    Adjust the arbitrary user’s responses as necessary to maintain a coherent conversation that highlights the shift in the INFP character’s personality traits. 
-    The context of the dialogue remains the same, but the tone, approach, and underlying values in the responses from the INFP character should reflect these changes, ensuring a clear contrast to the original dialogue’s INFP characteristics.
-    The dialogue that you should invert is:
-    "{dialogue_to_invert}"
+    content = f'''Given a dialogue between an INFP individual and an arbitrary user, please invert the INFP's responses to reflect opposite personality traits.
+=    The context of the dialogue remains the same, but the tone, approach, and underlying values in the responses from the INFP character should reflect these changes, ensuring a clear contrast to the original dialogue’s INFP characteristics.
+    The user says: "{prompt}"
+    The INFP character says: "{chosen}"
 
-    Start the conversation with the following response from the User:
-    "{start_of_dialogue}"
+    Now, reverse the INFP character's response to reflect opposite personality traits. Answer in the format below:
+    Reversed response: "The INFP character's reversed response."
     '''
 
     message = client.messages.create(
@@ -41,60 +28,60 @@ def generate_dialgue(infp_dialogue):
         max_tokens=1000,
         temperature=1,
         messages=[
-        {"role": "user", "content": f"{prompt}"}
+            {"role": "user", "content": content}
         ]
     )
 
-    inverted_dialogue = message.content[0].text.splitlines()
-    inverted_dialogue = list(filter(lambda x: x != "", inverted_dialogue))
+    rejected = message.content[0].text
 
-    output = {"character": character, "scene": situation, "chosen": infp_dialogue["dialogue"], "rejected": inverted_dialogue}
+    start_index = rejected.find("\"")
+    end_index = rejected.rfind("\"")
+    if start_index != -1 and end_index != -1:
+        rejected = rejected[start_index + 1:end_index]
     
-    return output
+    return rejected
 
 
 
-with open("data/generated_data/infp_character_situation_dialogues.json", "r") as f:
-    infp_dialogues = json.load(f)
+with open("data/generated_data/entj_character_situation_dialogues.json", "r") as f:
+    dataset = json.load(f)
 
 load_dotenv()
 client = anthropic.Anthropic(
     api_key=os.environ.get("CLAUDE_API_KEY")
 )
 
-infp_dialogue_dataset = list()
-
 start = time.time()
 
-for i in range(0, len(infp_dialogues), 5):
-    print(f"Generating dialogue for character {i+1} to {i+5} of {len(infp_dialogues)}")
+dataset = preprocess_data(dataset)
+
+pair_data = []
+for i, data in enumerate(dataset):
+    print(f"Generating dialogue for character {i+1} of {len(dataset)}")
+    prompt, chosen = data["prompt"], data["chosen"]
+
     success = False
     error_count = 0
-
     while not success:
         try:
-            outputs = Parallel(n_jobs=3, verbose=100, prefer="threads")(
-                delayed(generate_dialgue)(
-                    infp_dialogue=infp_dialogue
-                )
-                for infp_dialogue in infp_dialogues[i: i+5]
-            )
-            for output in outputs:
-                infp_dialogue_dataset.append(output)
+            rejected = reverse_response(prompt, chosen)
             success = True
         except Exception as e:
-            print(e)
             error_count += 1
             if error_count >= 5:
-                with open("data/generated_data/infp_pair_data.json", "w") as f:
-                    json.dump(infp_dialogue_dataset, f, indent=4)
+                with open("data/generated_data/entj_pair_data_1.json", "w") as f:
+                    json.dump(pair_data, f, indent=4)
                 sys.exit("Too many errors, exiting")
             time.sleep(20)
 
+    pair_data.append({"prompt": prompt, "chosen": chosen, "rejected": rejected})
+
+    #if len(pair_data) > 4:
+    #    break
 
 
-with open("data/generated_data/infp_pair_data.json", "w") as f:
-    json.dump(infp_dialogue_dataset, f, indent=4)
+with open("data/generated_data/entj_pair_data_2.json", "w") as f:
+    json.dump(pair_data, f, indent=4)
 
 end = time.time()
 print(f"Time taken: {end-start}")
